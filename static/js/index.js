782fcf4c056f834c1c99bdc449fb69206fa8e4da
kesler <uchekesla@gmail.com>
2022-07-02 20:57:26 +0100

    enabled the creationg of multiple tables

--- static/js/index.js
+++ static/js/index.js
@@ -1,48 +1,117 @@
-const dragDiagrams = document.querySelectorAll(".diagram");
-const gridDiagram = document.querySelector(".uml");
-const btn = document.querySelector(".dragBtn");
+const objectName = document.querySelectorAll(".object-name").item(0);
+const dataModelCanvas = document.querySelector(".data-model-canvas");
+const createTableBtn = document
+  .getElementsByClassName("create-table-btn")
+  .item(0);
 
-dragDiagrams.forEach((diagram) => {
-  const drag = (e) => {
-    diagram.style.top = e.pageY + "px";
-    diagram.style.left = e.pageX + "px";
+class Line {
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
+  }
+  connect = () => {
+    const connection = document.createElement("svg");
   };
+}
+class UmlDiagramTable {
+  constructor(name) {
+    this.name = name;
+  }
 
-  /*function drag(e) {
+  createTable = () => {
+    const diagram = document.createElement("div");
+    diagram.classList.add("diagram");
 
-          }*/
+    const uml = document.createElement("div");
+    uml.classList.add("uml");
 
-  diagram.addEventListener("mousedown", () => {
-    window.addEventListener("mousemove", drag);
+    const gridTable = document.createElement("div");
+    gridTable.classList.add("grid-table");
+
+    const objectName = document.createElement("input");
+    objectName.classList.add("object-name");
+    objectName.type = "text";
+    objectName.style.borderTop = `25px solid ${getRandomColor()}`;
+    objectName.placeholder = "Object Name";
+
+    uml.appendChild(objectName);
+    uml.appendChild(gridTable);
+    diagram.appendChild(uml);
+    dataModelCanvas.appendChild(diagram);
+
+    enableDrag();
+    enableNavigation();
+  };
+}
+
+const getRandomNumber = (maxNum) => {
+  return Math.floor(Math.random() * maxNum);
+};
+
+const getRandomColor = () => {
+  const r = getRandomNumber(200);
+  const g = getRandomNumber(200);
+  const b = getRandomNumber(200);
+
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+const enableDrag = () => {
+  const dragDiagrams = document.querySelectorAll(".diagram");
+  dragDiagrams.forEach((diagram) => {
+    const drag = (e) => {
+      diagram.style.top = e.pageY + "px";
+      diagram.style.left = e.pageX + "px";
+    };
+
+    /*function drag(e) {
+      
+    }*/
+
+    diagram.addEventListener("mousedown", () => {
+      window.addEventListener("mousemove", drag);
+    });
+
+    window.addEventListener("mouseup", () => {
+      window.removeEventListener("mousemove", drag);
+    });
   });
+};
 
-  window.addEventListener("mouseup", () => {
-    window.removeEventListener("mousemove", drag);
-  });
-});
-
-const addRow = () => {
+const addRow = (e) => {
   let diagramRow1 = document.createElement("input");
   let diagramRow2 = document.createElement("input");
   let diagramRow3 = document.createElement("input");
-  
+  diagramRow1.value = "+";
+
   let arrowBtn = document.createElement("div");
   arrowBtn.classList.add("arrowBtn");
   let arrowBtnIcon = document.createElement("i");
   arrowBtnIcon.classList.add("fas");
   arrowBtnIcon.classList.add("fa-plus");
   arrowBtn.appendChild(arrowBtnIcon);
-  
-  umlTable = document.querySelector(".grid-table");
+
+  umlTable =
+    e.target.parentNode.classList.value === "grid-table"
+      ? e.target.parentNode
+      : e.target.parentNode.children.item(1);
+
   umlTable.appendChild(diagramRow1);
   umlTable.appendChild(diagramRow2);
   umlTable.appendChild(diagramRow3);
   umlTable.appendChild(arrowBtn);
-  diagramRow1.focus();
+  diagramRow2.focus();
+
+  arrowBtn.addEventListener('click', (e) => {
+    createLine(e)
+  })
 };
 
-const deleteRow = () => {
-  umlTable = document.querySelector(".grid-table");
+const deleteRow = (e) => {
+  umlTable =
+    e.target.parentNode.classList.value === "grid-table"
+      ? e.target.parentNode
+      : e.target.parentNode.children.item(1);
   umlTable.removeChild(umlTable.children.item(umlTable.children.length - 1));
   umlTable.removeChild(umlTable.children.item(umlTable.children.length - 1));
   umlTable.removeChild(umlTable.children.item(umlTable.children.length - 1));
@@ -62,36 +131,63 @@ const findIndex = (collection, item) => {
   }
 };
 
-dragDiagrams.forEach((inputElement) => {
-  inputElement.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-      addRow();
-    } else if (e.keyCode === 8 && e.target.value === "") {
-      deleteRow();
-    } else if (e.key === "ArrowRight") {
-      try {
-        let nextRow = e.target.parentNode.children.item(
-          findIndex(e.target.parentNode.children, e.target) + 1
-        );
-        nextRow.focus();
-      } catch (e) {
-        console.log(e);
+const enableNavigation = () => {
+  const dragDiagrams = document.querySelectorAll(".diagram");
+  dragDiagrams.forEach((inputElement) => {
+    inputElement.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        addRow(e);
+      } else if (e.keyCode === 8 && e.target.value === "") {
+        deleteRow(e);
+      } else if (e.key === "ArrowRight") {
+        try {
+          let nextRow = e.target.parentNode.children.item(
+            findIndex(e.target.parentNode.children, e.target) + 1
+          );
+          nextRow.focus();
+        } catch (e) {
+          console.log(e);
+        }
+      } else if (e.key === "ArrowLeft") {
+        try {
+          let nextRow = e.target.parentNode.children.item(
+            findIndex(e.target.parentNode.children, e.target) - 1
+          );
+          nextRow.focus();
+        } catch (e) {
+          console.log(e);
+        }
+      } else {
+        console.log(e.key);
       }
-    } else if (e.key === "ArrowLeft") {
-      try {
-        let nextRow = e.target.parentNode.children.item(
-          findIndex(e.target.parentNode.children, e.target) - 1
-        );
-        nextRow.focus();
-      } catch (e) {
-        console.log(e);
-      }
-    } else {
-      console.log(e.key);
-    }
+    });
   });
-});
+};
 
-btn.addEventListener("click", addRow);
+objectName.style.borderTop = `25px solid ${getRandomColor()}`;
 
+const createLine = (e) => {
+  let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("class", "octicon octicon-star");
+  svg.setAttribute("viewBox", "0 0 800 800");
+  svg.setAttribute("aria-hidden", "true");
+  
+  let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute("fill", "none");
+  path.setAttribute("stroke", "blue");
+  path.setAttribute(
+    "d",
+    `M ${e.pageX},${e.pageY} L 25, 0 Q 30, 0  30, 5 L 30, 25 Q 30, 30  35, 30 L 100, 30`
+  );
+  
+  svg.appendChild(path);
+  dataModelCanvas.appendChild(svg);
+}
+
+enableDrag();
+enableNavigation();
 // insert pluses as default inputs
+createTableBtn.addEventListener("click", () => {
+  table = new UmlDiagramTable("this");
+  table.createTable();
+});
